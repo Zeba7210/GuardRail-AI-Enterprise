a73420ef80708363bf69086eb69dd1a65f030f23
Zeba Mukhtar <zebamukhtar2003@gmail.com>
2026-06-25 00:14:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins:[
     react(),
-    tailwindcss // 👈 Humne Tailwind ka plugin Vite engine mein jod diya!
+    tailwindcss 
   ],
 })
